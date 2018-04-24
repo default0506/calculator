3f317119d652076eb2cdaa49aec9beb741f60cea
cloudsoftware <kalee7878@gmail.com>
2018-04-24 10:08:34 +0900

    Add caculator source and unit test

---
+++ src/test/java/com/cloudsoftware/calculator/CalculatorTest.java
@@ -0,0 +1,12 @@
+package com.cloudsoftware.calculator;
+import static org.junit.Assert.*;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+public class CalculatorTest{
+	private Calculator calculator = new Calculator();
+	
+	@Test
+	public void testSum() {
+		assertEquals(5, calculator.sum(2,3));
+	}
+}
